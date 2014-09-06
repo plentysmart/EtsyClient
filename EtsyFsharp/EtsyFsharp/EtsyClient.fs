67533b3881fba599cc19968b3639f3fdb32c1e61
plentysmart <github@plentysmart.co.uk>
2014-09-06 16:56:09 +0100

    Initial internal api

--- EtsyFsharp/EtsyFsharp/EtsyClient.fs
+++ EtsyFsharp/EtsyFsharp/EtsyClient.fs
@@ -1,10 +1,13 @@
 ﻿namespace EtsyFsharp
-
 open System
+module EtsyAPI =
+    let  formatToArray (ids: 'a list) =
+            String.Join(",",ids)
+    module StandardParameters =
+        open EtsyFsharp.Web
+        let  withApiKey = withQueryStringParameter "api_key"
+        let  withfields (fields:string list) = withQueryStringParameter "fields" (formatToArray fields)
+        let  withLimit (limit: int) = withQueryStringParameter "limit" (limit.ToString())
+        let  withOffset (offset: int) = withQueryStringParameter "offset" (offset.ToString())
 
-type EtsyClient(api_key) =
-    let apikey = api_key 
-
-    member x.GetListings() = 
-        raise (NotImplementedException())
-
+  
---
+++ EtsyFsharp/EtsyFsharp/Web.fs
@@ -0,0 +1,29 @@
+﻿namespace EtsyFsharp
+open System
+module Web = 
+    type HttpMethod = 
+        | GET
+        | POST
+        | DELETE
+        | PUT
+
+    type Request = { 
+        Path:string;
+        QueryStringParameters: ((string*string) list)
+        Method:HttpMethod;
+        Content:string;
+        }
+    type Header = {Name:string; Value:string}
+    type  Response = {
+        Request:Request;
+        Headers: Header list;
+        HttpCode: int;
+        TextContent: string; 
+    }
+    let  defaultRequest = {Path="";Method=HttpMethod.GET;Content="";QueryStringParameters=[]}
+
+    let  withPath (path:string) (request: Request ) =  {request with Path = path}
+    let  withQueryStringParameter (key:string) (value:string) (request: Request) ={ request with QueryStringParameters = (key,value)::request.QueryStringParameters}
+    let  withMethod (httpMethod:HttpMethod)(request: Request) = { request with Method = httpMethod}
+    let ExecuteRequest (request:Request) :Response =
+            raise (NotImplementedException())
